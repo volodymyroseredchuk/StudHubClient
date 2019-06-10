7883ac4c502a7c74d43fd22e165405875c12d5e6
Zap1999 <45321175+Zap1999@users.noreply.github.com>
2019-06-10 16:34:03 +0300

    main page moved to angular project

--- studhub/src/app/app.module.ts
+++ studhub/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HomeModule } from './home/home.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ studhub/src/assets/js/scripts.js
@@ -0,0 +1,80 @@
+function mainPageFunction() {
+  class Card{
+
+    constructor(element){
+      this.element = element;
+      this.elementLocked = false;
+      this.delayedClose = false;
+
+      this.init();
+    }
+
+    open(){
+      this.element.className = "card is-flipped";
+    }
+
+    close(){
+      this.element.className = "card";
+    }
+
+    lock(){
+      this.elementLocked = true;
+    }
+
+    unlock(timeout = null){
+      if(timeout){
+        setTimeout(()=>{
+          this.elementLocked = false;
+          this.unlockedEvent();
+        }, timeout);
+      }else{
+        this.elementLocked = false;
+        this.unlockedEvent();
+      }
+
+    }
+
+    unlockedEvent(){
+      if(this.delayedClose){
+        this.delayedClose = false;
+        this.close();
+      }
+    }
+
+    isLocked(){
+      return this.elementLocked;
+    }
+
+    isUnLocked(){
+      return !this.isLocked();
+    }
+
+    init(){
+      var thus = this;
+
+      this.element.addEventListener("mouseover",()=>{
+        if(thus.isUnLocked()){
+          thus.open();
+          thus.lock();
+          thus.unlock(1000);
+        }else{
+          thus.delayedClose = false;
+        }
+      });
+
+      this.element.addEventListener("mouseleave",()=>{
+        if(thus.isUnLocked()){
+          thus.close();
+        }else{
+          thus.delayedClose = true;
+        }
+      });
+    }
+  }
+
+var card = document.querySelectorAll('.card');
+
+for(var i = 0; i< card.length;i++){
+  new Card(card[i]);
+}
+}
