dcee06845c482be545290963e97e3f5719aa41e8
Rostyk Hlynka <hlynka1608@gmail.com>
2019-06-16 22:11:24 +0300

    signin/signup ui

--- studhub/src/app/auth/auth.module.ts
+++ studhub/src/app/auth/auth.module.ts
@@ -3,12 +3,21 @@ import { CommonModule } from '@angular/common';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { MaterialModule } from '../material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AlertComponent } from '../_components/alert.component';
 
 @NgModule({
-  declarations: [ SigninComponent, SignupComponent ],
+  declarations: [
+    SigninComponent,
+    SignupComponent,
+    AlertComponent
+  ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- studhub/src/app/auth/signup/signup.component.ts
+++ studhub/src/app/auth/signup/signup.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { AlertService } from '../../service/alert.service';
+import { AuthenticationService } from '../../service/authentication.service';
+import { UserService } from '../../service/user.service';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +13,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  loading = false;
+  submitted = false;
 
-  ngOnInit() {
+  constructor(
+      private formBuilder: FormBuilder,
+      private router: Router,
+      private authenticationService: AuthenticationService,
+      private userService: UserService,
+      private alertService: AlertService
+  ) { 
+      // redirect to home if already logged in
+      if (this.authenticationService.currentUserValue) { 
+          this.router.navigate(['/']);
+      }
   }
 
+  ngOnInit() {
+      this.registerForm = this.formBuilder.group({
+          firstName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(16)]],
+          lastName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(16)]],
+          email: ['', [Validators.required, Validators.email, Validators.maxLength(60)]],
+          username: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(16)]],
+          password: ['', [Validators.required, Validators.minLength(6)]]
+      });
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+      this.submitted = true;
+
+      // stop here if form is invalid
+      if (this.registerForm.invalid) {
+          return;
+      }
+
+      this.loading = true;
+      this.userService.register(this.registerForm.value)
+          .pipe(first())
+          .subscribe(
+              data => {
+                  this.alertService.success('Registration successful', true);
+                  this.router.navigate(['/signin']);
+              },
+              error => {
+                  this.alertService.error(error);
+                  this.loading = false;
+              });
+  }
 }
