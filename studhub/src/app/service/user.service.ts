d34dfe96a9fb8be8503bae59744adfda087a3236
Rostyk Hlynka <hlynka1608@gmail.com>
2019-06-16 22:11:04 +0300

    signin/signup service

---
+++ studhub/src/app/service/user.service.ts
@@ -0,0 +1,17 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BaseService } from './base-service';
+import { User } from '../model/user.model';
+
+@Injectable({ providedIn: 'root' })
+export class UserService extends BaseService {
+
+    constructor(protected http: HttpClient) {
+        super(http);
+     }
+
+    register(user: User) {
+        return this.http.post(`${this.apiUrl}/signup`, user);
+    }
+
+}       
