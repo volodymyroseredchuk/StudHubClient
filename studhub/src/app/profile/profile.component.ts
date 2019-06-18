9891295a3e63fb86eb6216253a640391853a8b06
DaNkOLULzz <danylolototskyi@gmail.com>
2019-06-18 23:14:38 +0300

    init styles fro profile page and create Profile and University models

--- studhub/src/app/profile/profile.component.ts
+++ studhub/src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Profile } from '../model/profile.model';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  profile: Profile;
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
+    this.getUser();
+  }
+
+  public getUser() {
+    let url = "http://localhost:8080/profile/my";
+    this.http.get<Profile>(url, {
+      headers: new HttpHeaders().
+        set('Authorization', localStorage.getItem('jwt-token'))
+    })
+      .subscribe(
+        res => {
+          this.profile = res;
+        },
+        err => {
+          alert('You are not logged in!');
+          this.router.navigate(['/signin'])
+        }
+      );
   }
 
 }
