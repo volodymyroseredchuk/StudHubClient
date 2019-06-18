4c5f607f170a0fa28c6a3874a735bbe73000bd9a
OlhaLozinska <olja.lviv133@gmail.com>
2019-06-18 21:24:18 +0300

    change configurations for Heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static('./dist/studhubclient'));
+app.use(express.static(__dirname + '/dist/studhubclient'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
