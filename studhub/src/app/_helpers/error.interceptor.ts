4c20ab037cffcde04fa30131af1bad7bc4c1bbe1
Rostyk Hlynka <hlynka1608@gmail.com>
2019-06-19 17:57:11 +0300

    fix

--- studhub/src/app/_helpers/error.interceptor.ts
+++ studhub/src/app/_helpers/error.interceptor.ts
@@ -17,7 +17,8 @@ export class ErrorInterceptor implements HttpInterceptor {
                 location.reload(true);
             }
             console.log(err);
-            const error = err.error.message || err.statusText;
+            
+            const error = err.error.message || err.error;
             return throwError(error);
         }))
     }
