3fe348531a7f7900e9e7d7e8cf90996404b76998
OlhaLozinska <olja.lviv133@gmail.com>
2019-06-22 16:27:46 +0300

    added feedback button

--- src/app/feedback/feedback-create/feedback-create.component.ts
+++ src/app/feedback/feedback-create/feedback-create.component.ts
@@ -16,6 +16,7 @@ export class FeedbackCreateComponent implements OnInit {
   addOnBlur = true;
   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
 
+  // tags: Tag[] = [];
   feedback: Feedback = this.feedbackMock();
 
   constructor(private feedbackService: FeedbackService) { }
@@ -26,19 +27,40 @@ export class FeedbackCreateComponent implements OnInit {
   feedbackMock(): Feedback {
     let feedback: Feedback = new Feedback();
     feedback.id = 0;
+    // feedback.answerList = [];
     feedback.body = "templateBody";
     feedback.creationDate = new Date();
+    // feedback.modifiedDate = null;
     feedback.title = "templateTitle";
     feedback.user = new User();
     feedback.user.id = 1;
 
+    // feedback.tagList = this.tags;
     return feedback;
   }
+
+  // addTag(event: MatChipInputEvent): void {
+  //   const input = event.input;
+  //   const value = event.value;
+  //
+  //   // Add tag
+  //   if ((value || '').trim()) {
+  //     this.tags.push({id:0, name: value.trim()});
+  //   }
 
     // Reset the input value
     if (input) {
       input.value = '';
     }
+  // }
+
+  // removeTag(tag: Tag): void {
+  //   const index = this.tags.indexOf(tag);
+  //
+  //   if (index >= 0) {
+  //     this.tags.splice(index, 1);
+  //   }
+  // }
 
   createFeedback() {
     this.feedbackService.createFeedback(this.feedbackMock())
