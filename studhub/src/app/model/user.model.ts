6e265c20b07b89f3fc4ce76725d5603840bc5bc7
Rostyk Hlynka <hlynka1608@gmail.com>
2019-06-23 22:04:24 +0300

    add univer

--- studhub/src/app/model/user.model.ts
+++ studhub/src/app/model/user.model.ts
@@ -1,3 +1,5 @@
+import { University } from './university.model';
+
 export class User {
     id: number;
     firstName: string;
@@ -5,4 +7,5 @@ export class User {
     username: string;
     password: string;
     email: string;
+    university: University;
 }
