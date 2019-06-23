a156709bd01393fdd7dd59a9040f8fe9f10af4a5
tarasgl <tarasgl@gmail.com>
2019-06-23 20:09:26 +0300

    fix commented files

--- studhub/src/app/_helpers/error.interceptor.ts
+++ studhub/src/app/_helpers/error.interceptor.ts
@@ -1,24 +1,22 @@
-// import { Injectable } from '@angular/core';
-// import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-// import { Observable, throwError } from 'rxjs';
-// import { catchError } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
-// import { AuthenticationService } from '../service/authentication.service';
-
-// @Injectable()
-// export class ErrorInterceptor implements HttpInterceptor {
-//     constructor(private authenticationService: AuthenticationService) {}
-
-//     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-//         return next.handle(request).pipe(catchError(err => {
-//             if (err.status === 403) {
-//                 // auto logout if 403 response returned from api
-//                 this.authenticationService.logout();
-//                 location.reload(true);
-//             }
-//             console.log(err);
-//             const error = err.error.message || err.statusText;
-//             return throwError(error);
-//         }))
-//     }
-// }
+import { AuthenticationService } from '../service/authentication.service';
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+    constructor(private authenticationService: AuthenticationService) {}
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(request).pipe(catchError(err => {
+            if (err.status === 403) {
+                // auto logout if 403 response returned from api
+                this.authenticationService.logout();
+                location.reload(true);
+            }
+            console.log(err);
+            const error = err.error.message || err.statusText;
+            return throwError(error);
+        }))
+    }
+}
