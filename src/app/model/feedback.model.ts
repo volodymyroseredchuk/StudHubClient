cbb2daf170b23fe61a00057b6d5ba862c9d2521e
OlhaLozinska <olja.lviv133@gmail.com>
2019-06-24 10:22:02 +0300

    added feedback body, mark, rating to all feedbacks page

--- src/app/model/feedback.model.ts
+++ src/app/model/feedback.model.ts
@@ -1,11 +1,15 @@
 import { Tag } from './tag.model';
 import { User } from './user.model';
 import { Answer } from './answer.model';
+import {University} from "./university.model";
+import {Teacher} from "./teacher.model";
 
 export class Feedback {
     id: number;
-    title: string;
     body: string;
-    creationDate: Date;
     user: User;
+    university: University;
+    teacher: Teacher;
+    mark: number;
+    rate: number;
 }
