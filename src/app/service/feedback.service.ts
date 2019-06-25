d29b4102987c1bb544afe56a80f8fee914e02933
OlhaLozinska <olja.lviv133@gmail.com>
2019-06-25 15:54:22 +0300

    added header to feedback

--- src/app/service/feedback.service.ts
+++ src/app/service/feedback.service.ts
@@ -2,36 +2,41 @@ import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 
 import {BaseService} from './base-service';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Feedback} from "../model/feedback.model";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class FeedbackService extends BaseService {
 
-  constructor(protected http: HttpClient) {
-    super(http);
-    this.apiUrl += '/feedback';
-  }
+    constructor(protected http: HttpClient) {
+        super(http);
+        this.apiUrl += '/feedback';
+    }
 
-  createFeedback(feedback: Feedback): Observable<Feedback> {
-    return this.http.post<Feedback>(`${this.apiUrl}/feedback`, feedback);
-  }
+    createFeedback(feedback: Feedback): Observable<Feedback> {
+        let headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Authorisation': `${localStorage.getItem("accessToken")}`
+        });
+        let options = {headers: headers};
+        return this.http.post<Feedback>(`${this.apiUrl}/feedback`, feedback, options);
+    }
 
-  // getAllFeedbackByTeacherId(teacherId: number, feedback: Feedback): Observable<Feedback[]> {
-  //     return this.http.get<Feedback[]>(`${this.apiUrl}`);
-  // }
+    // getAllFeedbackByTeacherId(teacherId: number, feedback: Feedback): Observable<Feedback[]> {
+    //     return this.http.get<Feedback[]>(`${this.apiUrl}`);
+    // }
 
-  getAllFeedbacks(): Observable<Feedback[]> {
-    return this.http.get<Feedback[]>(`${this.apiUrl}`);
-  }
+    getAllFeedbacks(): Observable<Feedback[]> {
+        return this.http.get<Feedback[]>(`${this.apiUrl}`);
+    }
 
-  editFeedback(feedbackId: number, feedback: Feedback): Observable<Feedback> {
-    return this.http.put<Feedback>(`${this.apiUrl}/${feedbackId}/edit`, feedback);
-  }
+    editFeedback(feedbackId: number, feedback: Feedback): Observable<Feedback> {
+        return this.http.put<Feedback>(`${this.apiUrl}/${feedbackId}/edit`, feedback);
+    }
 
-  deleteFeedback(feedbackId: number): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/${feedbackId}/delete`);
-  }
+    deleteFeedback(feedbackId: number): Observable<any> {
+        return this.http.delete(`${this.apiUrl}/${feedbackId}/delete`);
+    }
 }
