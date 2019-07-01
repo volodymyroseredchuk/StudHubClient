c571f62e5c71cb20fda654555d4942df932e5212
DaNkOLULzz <danylolototskyi@gmail.com>
2019-07-01 17:19:14 +0300

    add question list and init it in profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {User} from '../model/user.model';
 import {UserService} from '../service/user.service';
+import {QuestionForListDTO} from '../model/questionForListDTO.model';
 
 @Component({
   selector: 'app-profile',
@@ -9,16 +10,22 @@ import {UserService} from '../service/user.service';
 })
 export class ProfileComponent implements OnInit {
   user: User;
+  questions: QuestionForListDTO[];
 
   constructor(private userService: UserService) {
   }
 
   ngOnInit() {
     this.userService.getUser().subscribe(res => {
-      this.user = res
+      this.user = res;
     });
+    this.userService.getAllQuestionsByUser().subscribe(res => {
+      this.questions = res;
+    });
+
   }
 
 }
+
 
 
